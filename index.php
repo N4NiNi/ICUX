afb7bd5f3175740756c87fee8e52dc76288293d8
N4NiNi <vini_nanini@hotmail.com>
2023-10-25 15:08:20 -0300

    base feita

--- index.php
+++ index.php
@@ -3,7 +3,7 @@
   <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>Bootstrap demo</title>
+    <title>EUXT | Exploring User eXperience Tools</title>
 
     <link rel="stylesheet" type="text/css" href="styles\estilo.css">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
@@ -31,6 +31,62 @@
             </div>
         </nav>
     </header>
+    <div ">
+        <main id="teste">
+            <section class="d-flex align-items-center justify-content-center" id="home">
+                <iframe width="800" height="450" src="https://www.youtube.com/embed/G-C0Gqu9Ybw" title="Ferramentas computacionais voltadas ao usuário são tema de pesquisa na UFSCar" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen="" class="rounded-md"></iframe>
+            </section>
+            
+            <section class="d-flex justify-content-center" id="uxtoolkit">
+                <div id="teste" class="row">
+                    <h2 class="text-center">UX ToolKit</h2>
+                    <img class="col-6" alt src="imgs\illustration1.6dc90c9a.svg" width="664" height="463" decoding="async" data-nimg="1" loading="lazy" style="color:transparent">
+                    <div class="col-6 d-flex flex-column justify-content-center align-items-center"> 
+                        <p class="">The UX Toolkit is a collection of tools for different stages of the user experience design process. You can select a specific stage of your project, such as research, ideation, prototyping, or evaluation, and receive a set of recommended tools that can be used during that stage. Additionally, the UX Toolkit includes the Helper UX feature that allows you to answer a series of questions and receive a tailored set of tools based on your responses.</p>
+                        <a target="_blank" href="/uxtoolkit"><button class="btn_ux btn btn-outline-secondary">UX ToolKit</button></a>
+                    </div>
+                </div>
+            </section>
+            <section class="d-flex align-items-center justify-content-center" id="about">
+                
+                <div id="teste" class="row">
+                    <h2 class="text-center">About</h2>
+                    <img class="col-6" alt="" src="imgs\uxLerisLogo.15a026cd_2.svg" width="340" height="340" decoding="async" data-nimg="1" loading="lazy" style="color:transparent">
+                    <div class="col-6 d-flex flex-column justify-content-center align-items-center">
+                    <p>User eXperience – Laboratory of Studies in Networks, Innovation and Software is a research group that belongs to LERIS. LERIS is the main laboratory and it is localized at Department of Computer Science in the Federal University of São Carlos – Sorocaba, Brazil (UFSCar). UXLeris researchers work to approximate academic research to practical issues in the software industry by conducting experimental and empirical studies. </p> 
+                    <a target="_blank" href="http://uxleris.sor.ufscar.br/"><button class="btn btn-outline-secondary btn_ux">More Info</button></a>
+                        
+                    </div>
+                </div>
+            </section>
+        </main>
+        <footer class="rodape">
+            <div class="container">
+                <div class="row align-items-center">
+
+                    <div class="col">
+                        <div class="logo_bloco mr-auto">
+                            <img src="imgs\uxLerisLogo.15a026cd.svg" alt="Logo" class="navbar-logo">
+                            <div class="texto_logo">
+                            <h1 class="titulo_logo">EUXT</h1>
+                            <p class="desc_logo">Exploring User eXperience Tools</p>
+                        </div>
+                        
+                    </div>
+                    </div>
+
+                    <div class="col">
+                    <p class="text-center ">© EUXT 2023 - All Rights Reserved.</p>
+                    </div>
+
+                    <div class="col">
+                        <p class="text-center">BEETS</p>
+                    </div>
+
+                </div>
+            </div>
+        </footer>
+    </div>
     
 
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
